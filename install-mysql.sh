7b3325ed37b2d6844c16170a7f6341efa03baa58
m1028639 <sriprasanna.p@gmail.com>
2015-07-14 14:06:29 +0530

    Update install-mysql.sh

--- install-mysql.sh
+++ install-mysql.sh
@@ -15,5 +15,4 @@ apt-get -y install mysql-server
 sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
 
 sudo service mysql restart
-
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'welcome123'; flush privileges;" | mysql -u root -pwelcome123
+echo "CREATE DATABASE sonar CHARACTER SET utf8 COLLATE utf8_general_ci; CREATE USER 'sonar' IDENTIFIED BY 'sonar';GRANT ALL PRIVILEGES ON sonar.* TO 'sonar'@'%' IDENTIFIED BY 'sonar'; GRANT ALL ON sonar.* TO 'sonar'@'localhost' IDENTIFIED BY 'sonar'; flush privileges;" | mysql -u root -pwelcome123
