ec1cb9bbebb6216d2da2963ed42e1ebfb3ee2252
m1028639 <sriprasanna.p@gmail.com>
2015-07-14 14:03:30 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -2,4 +2,6 @@
 apt-get update
 wget -qO- https://get.docker.com/ | sh
 #Run Docker
-docker --version
+docker run --name sonar1 -d -p 9000:9000 -e DB_USER=sonar -e DB_PWD=sonar -e DB_URL=clientmysqldb.westus.cloudapp.azure.com -e DB_PORT=3306  preetick/sonar-devops:v18
+
+
