6aff73acaf78add91d82ffdd563059bcdc8bc16d
m1028639 <sriprasanna.p@gmail.com>
2015-07-14 14:04:53 +0530

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -5,4 +5,5 @@ apt-get update
 wget -qO- https://get.docker.com/ | sh
 
 
-docker --version
+docker run --name nginx_devops -d -p 80:80 -e NGINX_ADDR=clientnginx01.westus.cloudapp.azure.com -e JENKINS_ADDR=clientjenkins01.westus.cloudapp.azure.com -e SONAR_ADDR=clientsonar01.westus.cloudapp.azure.com preetick/nginx-devops:v12
+
