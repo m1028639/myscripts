556cf8be3a24e7622b9af01de7c83c7bb898a9e6
m1028639 <sriprasanna.p@gmail.com>
2015-07-15 12:19:07 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,5 +4,5 @@ apt-get update
 wget -qO- https://get.docker.com/ | sh
 
 
-docker run --name myjenkins -d -p 8080:8080 -p 50000:50000 -v /var/jenkins_home  preetick/jenimg_2jul2015:v4
+docker run --name myjenkins -d -p 8080:8080 -p 50000:50000 -v /var/jenkins_home preetick/jenkins-devops:v3
 
