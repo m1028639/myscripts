0f6f5f793663b8ba1bab137b889d25ccdb39d4e1
m1028639 <sriprasanna.p@gmail.com>
2015-08-20 15:48:59 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -2,6 +2,6 @@
 apt-get update
 wget -qO- https://get.docker.com/ | sh
 #Run Docker
-docker run --name sonar1 -d -p 9000:9000 -e DB_USER=sonar -e DB_PWD=sonar -e DB_URL=orchardmysqldb01.southeastasia.cloudapp.azure.com -e DB_PORT=3306  preetick/sonar-devops:v18
+docker run --name sonar1 -d -p 9000:9000 -e DB_USER=sonar -e DB_PWD=sonar -e DB_URL=clientmysqldb01.southeastasia.cloudapp.azure.com -e DB_PORT=3306  preetick/sonar-devops:v18
 
 
