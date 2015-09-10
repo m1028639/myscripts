a92c3d31e683a8e78c535b44ce8352fa5a0d3146
m1028639 <sriprasanna.p@gmail.com>
2015-09-10 23:41:43 +0530

    Create newsonar.sh

---
+++ newsonar.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get update
+wget -qO- https://get.docker.com/ | sh
+#Run Docker
+docker run --name sonar1 -d -p 9000:9000 -e DB_USER=sonar -e DB_PWD=sonar -e DB_URL=10.0.3.7 -e DB_PORT=3306  preetick/sonar-devops:v18
+
