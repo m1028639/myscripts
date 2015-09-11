a37a411d2deb182bc650ebb332929bea1965651d
m1028639 <sriprasanna.p@gmail.com>
2015-09-11 11:16:32 +0530

    Update newnginx.sh

--- newnginx.sh
+++ newnginx.sh
@@ -4,4 +4,4 @@ apt-get update
 wget -qO- https://get.docker.com/ | sh
 
 
-docker run --name nginx_devops -d -p 80:80 -e NGINX_ADDR=mtreemtnginx01.southeastasia.cloudapp.azure.com -e JENKINS_ADDR=http://10.0.2.8:8080/jenkins -e SONAR_ADDR=http://10.0.2.7:9000/sonar preetick/nginx-devops:v13
+docker run --name nginx_devops -d -p 80:80 -e NGINX_ADDR="$1.southeastasia.cloudapp.azure.com" -e JENKINS_ADDR=10.0.2.8 -e SONAR_ADDR=10.0.2.7 preetick/nginx-devops:v13
